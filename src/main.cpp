acfa56d2520992f28553174a6317935cc469f7bf
Jesus Hermosilla <hermosilladiaz@gmail.com>
2023-03-14 10:30:00 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,40 +11,40 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 
-#include "pose_est/ImageBoundingBox.h"
-pose_est::ImageBoundingBox bbox_msg;
+#include "uv_msgs/ImageBoundingBox.h"
+uv_msgs::ImageBoundingBox bbox_msg;
 
 using namespace std;
 
 /* Global variables */
 #define humanFrameID "human_detected"
-#define fixedFrameID "camera_link"
+#define fixedFrameID "kinect2_link"
 
 ros::Subscriber bbox_sub;
 ros::Subscriber pcd_sub;
 ros::Publisher pose_pub;
 
 vector<vector<float>> VectPose;
+bool ValidPose = false;
+int u_px, v_px;
+ros::Time bbox_time;
 
-class HumanParameters {
-public:
-  float Xposition;
-  float Yposition;
-  float Zposition;
-  float Velocity;
-  float Time;
-  pose_est::ImageBoundingBox Bbox;
-
-  void addBbox(const pose_est::ImageBoundingBox bbox_msg) {
-    Bbox = bbox_msg;
+void bboxCallback(const uv_msgs::ImageBoundingBox& msg_bbox) { 
+  if (isnan(msg_bbox.cornerPoints[0].u) == false) {
+    u_px = msg_bbox.cornerPoints[0].u + msg_bbox.width/2;
+    v_px = msg_bbox.cornerPoints[0].v + msg_bbox.height/2;
+    //bbox_time = msg_bbox.header.stamp;
+    ValidPose = true;
   }
-  void addPose(const sensor_msgs::PointCloud2ConstPtr& pCloud, const ros::Time& RosTime) {
-    /* Pixel coordinates in the point cloud */   
-    float X = 0.0;
-    float Y = 0.0;
-    float Z = 0.0;    
-    int u_px = Bbox.center.u + Bbox.width/2;
-    int v_px = Bbox.center.v + Bbox.height/2;
+}
+
+void pcdCallback(const sensor_msgs::PointCloud2ConstPtr& pCloud) {
+
+  float X = 0.0;
+  float Y = 0.0;
+  float Z = 0.0;
+
+  if (ValidPose == true) {
     int arrayPosition = v_px*pCloud->row_step + u_px*pCloud->point_step;
     int arrayPosX = arrayPosition + pCloud->fields[0].offset;
     int arrayPosY = arrayPosition + pCloud->fields[1].offset;
@@ -54,13 +54,13 @@ public:
     memcpy(&Y, &pCloud->data[arrayPosY], sizeof(float));
     memcpy(&Z, &pCloud->data[arrayPosZ], sizeof(float));
 
-    /* Pointcloud to plane projection */
     static tf2_ros::TransformBroadcaster br;
     geometry_msgs::TransformStamped transformStamped;
-    
-    transformStamped.header.stamp = RosTime;
-    transformStamped.header.frame_id = fixedFrameID;
-    transformStamped.child_frame_id = humanFrameID;
+    geometry_msgs::PoseStamped humanPose;
+
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = pCloud->header.frame_id;
+    transformStamped.child_frame_id = "human_detected";
     transformStamped.transform.translation.x = X;
     transformStamped.transform.translation.y = Y;
     transformStamped.transform.translation.z = Z;
@@ -70,54 +70,14 @@ public:
     transformStamped.transform.rotation.y = q.y();
     transformStamped.transform.rotation.z = q.z();
     transformStamped.transform.rotation.w = q.w();
+
     br.sendTransform(transformStamped);
-
-    Xposition = X;
-    Yposition = Y;
-    Zposition = Z;
-    Time = RosTime.toSec();
-  }
-  
-  void addVelocity(vector<vector<float>> VectPose) {
-    /* Point velocity */
-    float x1 = VectPose[0][0];
-    float y1 = VectPose[0][1];
-    float z1 = VectPose[0][2];
-    float x2 = VectPose[1][0];
-    float y2 = VectPose[1][1];
-    float z2 = VectPose[1][2];
-    float deltaT = VectPose[1][3] - VectPose[0][3]; 
-    Velocity = sqrt(pow(x2-x1,2) + pow(y2-y1,2) + pow(z2-z1,2))/deltaT;
-  }
-};
-
-void publish_point(const float X, const float Y, const float Z, const ros::Time& rosTime, const float Time, const float Velocity) {
-  geometry_msgs::PoseStamped humanPose;
-  humanPose.header.stamp = rosTime;
-  humanPose.header.frame_id = humanFrameID;
-  humanPose.pose.position.x = X; //red
-  humanPose.pose.position.y = Y; //green
-  humanPose.pose.position.z = Z; //blue
-  
-  /* Publish position */
-  pose_pub.publish(humanPose);
-  
-}
-
-void bboxCallback(const pose_est::ImageBoundingBox& msg_bbox) {
-  HumanParameters Hp;
-  Hp.addBbox(msg_bbox);
-}
-
-void pcdCallback(const sensor_msgs::PointCloud2ConstPtr& pCloud) { 
-  HumanParameters Hp;
-  ros::Time rostime = ros::Time::now();
-  Hp.addPose(pCloud, rostime); 
-  VectPose.push_back({Hp.Xposition, Hp.Yposition, Hp.Zposition, Hp.Time});
-  if (VectPose.size() == 2) {
-    Hp.addVelocity(VectPose);
-      publish_point(Hp.Xposition, Hp.Yposition, Hp.Zposition, rostime, Hp.Time, Hp.Velocity);
-      VectPose.erase(VectPose.begin());
+    humanPose.header.stamp = ros::Time::now();
+    humanPose.header.frame_id ="human_detected";
+    humanPose.pose.position.x = X; //red
+    humanPose.pose.position.y = Y; //green
+    humanPose.pose.position.z = Z; //blue
+    pose_pub.publish(humanPose);
   }
 }
 
@@ -126,9 +86,9 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh;
 
   bbox_sub = nh.subscribe("/humanBBox",1000,bboxCallback);
-  pcd_sub = nh.subscribe("/camera/depth_registered/points", 1000, pcdCallback);
+  pcd_sub = nh.subscribe("/kinect2/qhd/points", 1000, pcdCallback);
 
-  pose_pub = nh.advertise<geometry_msgs::PoseStamped>("human/position", 1);
+  pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/human/position", 1);
   
   while (ros::ok()) {
     ros::spinOnce();
